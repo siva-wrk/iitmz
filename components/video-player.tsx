bbf4fc4035f56117f0480429e71c3565aba97e24
Alwin <alwin@Alwins-MacBook-Air.local>
2023-07-17 20:40:31 +0530

    added loader skelton for video player and other minor css tweaks

--- components/video-player.tsx
+++ components/video-player.tsx
@@ -1,14 +1,17 @@
 "use client";
 import ReactPlayer from "react-player/youtube";
+import styles from "./video-player.module.scss";
 
 export default function VideoPlayer({ url }: { url: string }) {
   return (
-    <ReactPlayer
-      url={`https://www.youtube.com/watch?v=${url}`}
-      light={true}
-      height="340px"
-      width="calc(50% - 1.3rem)"
-      playing={true}
-    />
+    <div className={styles.skelton}>
+      <ReactPlayer
+        url={`https://www.youtube.com/watch?v=${url}`}
+        light={true}
+        height="340px"
+        width="100%"
+        playing={true}
+      />
+    </div>
   );
 }
