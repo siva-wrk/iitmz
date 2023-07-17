0e2600a1f1353357755d1277117e805d0819176c
Alwin <alwin@Alwins-MacBook-Air.local>
2023-07-17 19:06:18 +0530

    changed youtube video player lib

--- app/media/page.tsx
+++ app/media/page.tsx
@@ -1,7 +1,7 @@
 import styles from "./page.module.scss";
 import media from "@/data/media";
-import YouTubePlayer from "@/components/youtube-player";
 import NewsCard from "@/components/news-card";
+import VideoPlayer from "@/components/video-player";
 
 export default function MediaPage() {
   return (
@@ -9,7 +9,7 @@ export default function MediaPage() {
       <div className={styles.section}>
         <div className={styles.youtube_container}>
           {media.yt.map((video) => (
-            <YouTubePlayer key={video.id} videoId={video.id} />
+            <VideoPlayer key={video.id} url={video.id} />
           ))}
         </div>
         <div className={styles.news_container}>
