78adaf32941d06f29e0b70f38d4afe504d7d252a
Alwin <alwin@Alwins-MacBook-Air.local>
2023-07-18 10:04:55 +0530

    added favicons and removed ununsed footer import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import { Lato, Raleway } from "next/font/google";
 import Header from "@/components/header";
-import Footer from "@/components/footer";
+// import Footer from "@/components/footer";
 import Script from "next/script";
 
 const lato = Lato({
