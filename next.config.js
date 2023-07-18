93dcdc52c0385366cc27845055168b8bb9877555
Alwin <alwin@Alwins-MacBook-Air.local>
2023-07-18 15:57:35 +0530

    rewrite old broken resource links

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@ import withPlaiceholder from "@plaiceholder/next";
 /** @type {import('next').NextConfig} */
 
 let config = {
+  async rewrites() {
+    return [
+      {
+        source: "/zanzibar/:path",
+        destination: "/:path",
+      },
+    ];
+  },
   images: {
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
