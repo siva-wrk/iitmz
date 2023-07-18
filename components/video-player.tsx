9b8f4c43a59cfc2cff936ebc821daf6299008a7e
Alwin <alwin@Alwins-MacBook-Air.local>
2023-07-18 14:15:01 +0530

    youtube thumbnail refactor

--- components/video-player.tsx
+++ components/video-player.tsx
@@ -1,13 +1,30 @@
 "use client";
+import { useState } from "react";
 import ReactPlayer from "react-player/youtube";
+import Image from "next/image";
 import styles from "./video-player.module.scss";
 
-export default function VideoPlayer({ url }: { url: string }) {
+type VideoPlayerProps = {
+  id: string;
+  previewImage: string;
+};
+
+export default function VideoPlayer({ id, previewImage }: VideoPlayerProps) {
+  const [previewLoaded, setPreviewLoaded] = useState(false);
   return (
-    <div className={styles.skelton}>
+    <div className={!previewLoaded ? styles.skelton : styles.bg}>
       <ReactPlayer
-        url={`https://www.youtube.com/watch?v=${url}`}
-        light={true}
+        url={`https://www.youtube.com/watch?v=${id}`}
+        light={
+          <Image
+            src={`data:image/png;base64,${previewImage}`}
+            alt="Thumbnail Image"
+            fill={true}
+            style={{ objectFit: "cover" }}
+            priority={true}
+            onLoadingComplete={() => setPreviewLoaded(true)}
+          />
+        }
         height="100%"
         width="100%"
         playing={true}
