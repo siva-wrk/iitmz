d623278229d167ac233104a24425960e61ccdba0
Alwin <alwin@Alwins-MacBook-Air.local>
2023-07-25 14:36:51 +0530

    added faculty page

--- app/schools/engineering-and-science/page.tsx
+++ app/schools/engineering-and-science/page.tsx
@@ -4,6 +4,7 @@ import Img from "@/components/image";
 import { FaUniversity } from "react-icons/fa";
 import Button from "@/components/button";
 import Link from "next/link";
+import faculty from "@/data/faculty";
 
 export default function AcademicsPage() {
   return (
@@ -79,84 +80,23 @@ export default function AcademicsPage() {
         <div id="faculty" className={styles.faculty}>
           <h2 className="section-title center line">Faculty</h2>
           <div>
-            <div className={styles.card}>
-              <Img
-                src="/ligy-1.png"
-                alt="Photo of Prof Ligy Phillip"
-                width={200}
-                height={200}
-              />
-              <h3>Prof. Ligy Phillip</h3>
-              <h4>Chair Professor, Civil Engg</h4>
-              <Link
-                target="_blank"
-                href="https://civil.iitm.ac.in/faculty/ligy/"
-              >
-                <Button kind="SECONDARY">Know More</Button>
-              </Link>
-            </div>
-
-            <div className={styles.card}>
-              <Img
-                src="/ramkrishna.png"
-                alt="Photo of Prof Ramkrishna Pasumarthy"
-                width={200}
-                height={200}
-              />
-              <h3>Prof. Ramkrishna Pasumarthy</h3>
-              <h4>Program Chair, BS DS & AI</h4>
-              <Link
-                target="_blank"
-                href="https://www.ee.iitm.ac.in/ramkrishna/"
-              >
-                <Button kind="SECONDARY">Know More</Button>
-              </Link>
-            </div>
-            <div className={styles.card}>
-              <Img
-                src="/nirav-1.jpg"
-                alt="Photo of Prof Nirav Bhatt"
-                width={200}
-                height={200}
-              />
-              <h3>Dr. Nirav Bhatt</h3>
-              <h4>Program Chair, MTech DS & AI</h4>
-              <Link
-                target="_blank"
-                href="https://biotech.iitm.ac.in/faculty/nirav-p-bhatt/"
-              >
-                <Button kind="SECONDARY">Know More</Button>
-              </Link>
-            </div>
-            <div className={styles.card}>
-              <Img
-                src="/raghunathan-1.jpg"
-                alt="Photo of Prof Raghunathan Rengaswamy"
-                width={200}
-                height={200}
-              />
-              <h3>Prof. Raghunathan Rengaswamy</h3>
-              <h4>Chair Professor, Chemical Engg</h4>
-              <Link
-                target="_blank"
-                href="https://ibse.iitm.ac.in/people/raghunathan-rengaswamy/"
-              >
-                <Button kind="SECONDARY">Know More</Button>
-              </Link>
-            </div>
-            <div className={styles.card}>
-              <Img
-                src="/preeti-a.jpg"
-                alt="Photo of Prof Preeti Aghalayam"
-                width={200}
-                height={200}
-              />
-              <h3>Prof. Preeti Aghalayam</h3>
-              <h4>Professor, Chemical Engg</h4>
-              <Link target="_blank" href="https://che.iitm.ac.in/preeti/">
-                <Button kind="SECONDARY">Know More</Button>
-              </Link>
-            </div>
+            {faculty.map((facultyMember) => (
+              <div className={styles.card}>
+                <Img
+                  src={facultyMember.img}
+                  alt={`Photo of ${facultyMember.name}`}
+                  width={200}
+                  height={200}
+                />
+                <h3>{facultyMember.name}</h3>
+                <h4>{facultyMember.title}</h4>
+                <Link
+                  href={`/schools/engineering-and-science/faculty/${facultyMember.slug}`}
+                >
+                  <Button kind="SECONDARY">Know More</Button>
+                </Link>
+              </div>
+            ))}
           </div>
         </div>
       </div>
---
+++ data/faculty.js
@@ -0,0 +1,378 @@
+const faculty = [
+  {
+    slug: "prof-ramkrishna-pasumarthy",
+    name: "Prof. Ramkrishna Pasumarthy",
+    img: "/ramkrishna.png",
+    title: "Program Chair, BS DS & AI",
+    desc: "",
+    contact: {
+      email: "ramkrishna@ee.iitm.ac.in",
+      website: "www.ee.iitm.ac.in/ramkrishna/",
+    },
+    interests:
+      "My interests lie in the area of modeling and control of complex physical systems. I also have interests in the area of identification and control of (cloud) computing systems and analysis and control of power, traffic, brain and cloud networks.",
+    qualification: [
+      {
+        title: "PhD",
+        year: "2006",
+        course: "Applied Mathematics",
+        place: "",
+      },
+      {
+        title: "MTech",
+        year: "2001",
+        course: "Electrical Engineering",
+        place: "VJTI, Mumbai",
+      },
+      {
+        title: "BTech (with honors)",
+        year: "1999",
+        course: "Electrical Engineering",
+        place: "Bhilai Institute of Technology",
+      },
+    ],
+    papers: [
+      {
+        title:
+          "Model-Free Control Design for Feedback-Linearizable SISO Systems",
+        authors: ["K Shenoy", "A Saradagi", "R Pasumarthy", "V Chellaboina"],
+        publisher: "arXiv preprint",
+        year: "2023",
+      },
+      {
+        title:
+          "End-to-end Autonomous Driving in Heterogeneous Traffic Scenario Using Deep Reinforcement Learning",
+        authors: ["S Chakraborty", "S Kumar", "N Bhatt", " R Pasumarthy"],
+        publisher: "European Control Conference ",
+        year: "2023",
+      },
+      {
+        title:
+          "Data-Driven Feedback Linearization of Nonlinear Systems with Periodic Orbits in the Zero-Dynamics",
+        authors: ["K Shenoy", "A Saradagi", "R Pasumarthy", "V Chellaboina"],
+        publisher: "arXiv preprint",
+        year: "2023",
+      },
+      {
+        title:
+          "On quantification and maximization of information transfer in network dynamical systems",
+        authors: ["MS Singh", "R Pasumarthy", "U Vaidya", "S Leonhardt"],
+        publisher: "Scientific Reports",
+        year: "2023",
+      },
+    ],
+  },
+  {
+    slug: "dr-nirav-bhatt",
+    name: "Dr. Nirav Bhatt",
+    img: "/nirav-1.jpg",
+    title: "Program Chair, MTech DS & AI",
+    desc: "",
+    interests:
+      "In my research, I explore biological and disease networks using multi-sensor and multi-scale data. I focus on modeling, control, optimization, and integration of (Bio-)process Manufacturing to enhance efficiency. I also investigate bioprocess control and process analytical technology for quality assurance. Additionally, I study complex man-made networks through data-driven learning and control approaches. My goal is to advance understanding and management of these networks.",
+    contact: {
+      email: "niravbhatt@iitm.ac.in",
+      website: "biotech.iitm.ac.in/faculty/nirav-p-bhatt",
+    },
+    qualification: [
+      {
+        title: "PhD",
+        year: "2011",
+        course: "Doctoral School of Computer and Communication Sciences",
+        place: "EPFL, Switzerland",
+      },
+      {
+        title: "MTech",
+        year: "2005",
+        course: "Chemical Engineering",
+        place: "IIT, Madras",
+      },
+      {
+        title: "BE",
+        year: "2003",
+        course: "Chemical Engineering",
+        place: "The M S University of Baroda",
+      },
+    ],
+    papers: [
+      {
+        title:
+          "End-to-end Autonomous Driving in Heterogeneous Traffic Scenario Using Deep Reinforcement Learning",
+        authors: [
+          "Chakraborty, S.",
+          "Kumar, S.",
+          "Bhatt, N.",
+          "Pasumarthy, R.",
+        ],
+        publisher: "European Control Conference",
+        year: "2023",
+      },
+      {
+        title:
+          "Chemistry Inspired Molecular Representation for Property Prediction.",
+        authors: ["Gedela, G. S.", "Bobby, J.", "Bhatt, N."],
+        publisher:
+          "5th Joint International Conference on Data Science & Management of Data",
+        year: "2022",
+      },
+      {
+        title:
+          "Verification and Rectification of Error in Topology of Conserved Networks",
+        authors: ["Bawa, A.", "Pappu Jayaved S.", "Bhatt, N."],
+        publisher: "IFAC-PapersOnLine",
+        year: "2022",
+      },
+      {
+        title:
+          "Incremental Model Identification of Bio-processes from Data: Application to Microbial Production of Hyaluronic Acid",
+        authors: ["Kamakshi, C.", "Jayaraman, G.", "Bhatt, N.P."],
+        publisher: "IFAC-PapersOnLine",
+        year: "2022",
+      },
+      {
+        title:
+          "Aided Selection of Sampling Methods for Imbalanced Data Classification.",
+        authors: ["Sahni, D.", "Pappu, S. J.", "Bhatt, N."],
+        publisher: "8th ACM IKDD CODS & 26th COMAD",
+        year: "2021",
+      },
+      {
+        title:
+          "Thermodynamically consistent estimation of Gibbs free energy from data: data reconciliation approach.",
+        authors: ["Salike, S.", "Bhatt, N."],
+        publisher: "Bioinformatics",
+        year: "2020",
+      },
+      {
+        title:
+          "Aided Selection of Sampling Methods for Imbalanced Data Classification.",
+        authors: ["Jain, R.", "Narasimhan, S.", "Bhatt, N. P."],
+        publisher: "Automatica",
+        year: "2019",
+      },
+      {
+        title:
+          "Analysis of experimental conditions, measurement strategies, and model identification approaches on parameter estimation in plug flow reactors.",
+        authors: [
+          "Manokaran, V.",
+          "Sengupta, T.",
+          "Narasimhan, S.",
+          "Bhatt, N.",
+        ],
+        publisher: "Ind Eng Chem Res",
+        year: "2019",
+      },
+      {
+        title:
+          "Identifying topology of low voltage distribution networks based on smart meter data.",
+        authors: ["Pappu, S. J.", "Bhatt, N.", "Pasumarthy, R.", "Rajeswaran"],
+        publisher: "EEE Transactions on Smart Grid",
+        year: "2017",
+      },
+    ],
+  },
+  {
+    slug: "prof-raghunathan-rengaswamy",
+    name: "Prof. Raghunathan Rengaswamy",
+    img: "/raghunathan-1.jpg",
+    title: "Chair Professor, Chemical Engg",
+    desc: "",
+    interests:
+      "My research Area’s are Reinforcement Learning, Big Data Process Analytics, Machine Learning & Big Data.",
+    contact: {
+      email: "raghur@iitm.ac.in",
+      website: "ibse.iitm.ac.in/people/raghunathan-rengaswamy/",
+    },
+    qualification: [
+      {
+        title: "PhD",
+        year: "1995",
+        course: "Chemical Engineering",
+        place: "Purdue University,",
+      },
+      {
+        title: "BTech",
+        year: "1990",
+        course: "Chemical Engineering",
+        place: "IIT Madras",
+      },
+    ],
+    papers: [
+      {
+        title:
+          "Data science and IoT based mobile monitoring framework for hyper-local PM2. 5 assessment in urban setting",
+        authors: [
+          " S Swaminathan",
+          "AVS Guntuku",
+          "S Sumeer",
+          " A Gupta",
+          "R Rengaswamy",
+        ],
+        publisher: "Building and Environment 225",
+        year: "2022",
+      },
+      {
+        title: "Integration of Machine Learning and First Principles Models",
+        authors: ["L.Rajulapati", "S.Chinta", "B.Shyamala", "R.Rengaswamy"],
+        publisher: "AIChE J",
+        year: "2022",
+      },
+      {
+        title: "Reinforcement-Learning designs droplet microfluidic networks",
+        authors: ["M Shahab", "R Rengaswamy"],
+        publisher: "Computers and Chemical Engineering",
+        year: "2022",
+      },
+      {
+        title: "CDiNN -Convex Difference Neural Networks",
+        authors: ["Parameswaran Sankaranarayanan", "Raghunathan Rengaswamy"],
+        publisher: "Neurocomputing",
+        year: "2022",
+      },
+    ],
+  },
+  {
+    slug: "prof-preeti-aghalayam",
+    name: "Prof Preeti Aghalayam",
+    img: "/preeti-a.jpg",
+    title: "Professor, Chemical Engg",
+    desc: "",
+    interests:
+      "My interest areas include Underground Coal Gasification, Reduction of Automotive NOx, Reduction of Large Reaction Mechanisms, Reactor Modeling",
+    contact: {
+      email: "preeti@iit.ac.in",
+      website: "che.iitm.ac.in/preeti/",
+    },
+    qualification: [
+      {
+        title: "PhD",
+        year: "2000",
+        course: "Chemical Engineering",
+        place: "University of Massachusetts, Amherst, USA",
+      },
+      {
+        title: "M.S",
+        year: "1995",
+        course: "Chemical Engineering",
+        place: "University of Rochester, Rochester, USA",
+      },
+      {
+        title: "B.Tech",
+        year: "1991",
+        course: "Chemical Engineering",
+        place: "IIT Madras Chennai",
+      },
+    ],
+    papers: [
+      {
+        title:
+          "Unravelling the role of metal-metal oxide interfaces of Cu/ZnO/ZrO2/Al2O3 catalyst for methanol synthesis from CO2: Insights from experiments and DFT-based microkinetic modeling",
+        authors: [
+          "Dharmalingam, B C",
+          "V, A Koushik",
+          "Mureddu, M",
+          "Atzori, L",
+          "Lai, S",
+          "Pettinau, A",
+          "Kaisare, N S",
+          "Aghalayam, P",
+          "Varghese, J J",
+        ],
+        publisher: "Applied Catalysis B: Environmental, 332",
+        year: 2023,
+      },
+      {
+        title:
+          "Optimizing the synthesis of Ag/γ-Al2O3 for selective reduction of NOx with C3H6: Experiments and modelling",
+        authors: [
+          "Kummari, S K",
+          "Yedala, N",
+          "Selvam, P",
+          "Kaisare, N S",
+          "Aghalayam, P",
+        ],
+        publisher: "Canadian Journal of Chemical Engineering",
+        year: 2023,
+      },
+      {
+        title:
+          "A methodology for structure dependent global kinetic models: Application to the selective catalytic reduction of NO by hydrocarbons",
+        authors: ["Yedala, N", "Aghalayam, P"],
+        publisher: "Chemical Engineering Research and Design, 181",
+        year: 2022,
+      },
+      {
+        title:
+          "Low-temperature NH3-SCR of NO over robust RuNi/Al-SBA-15 catalysts: Effect of Ru loading",
+        authors: ["Perumal, S K", "Kaisare, N", "Kummari, S K", "Aghalayam, P"],
+        publisher: "Journal of Environmental Chemical Engineering, 10 (5)",
+        year: 2022,
+      },
+    ],
+  },
+  {
+    slug: "prof-ligy-philip",
+    name: "Prof Ligy Philip",
+    img: "/ligy-1.png",
+    title: "Professor, Civil Engg",
+    desc: "",
+    interests:
+      "My Research interests lie in the fields of water treatment and rural water supply. I am particularly drawn to domestic and industrial wastewater treatment, with a strong focus on wastewater reuse and recycling. Additionally, I am fascinated by the concept of bioremediation, especially in regards to contaminated soils, air, and water containing heavy metals, pesticides, and other hazardous organic compounds. ",
+    contact: {
+      email: "ligy@iitm.ac.in",
+      website: "civil.iitm.ac.in/faculty/ligy/",
+    },
+    qualification: [
+      {
+        title: "PhD",
+        year: "1998",
+        course: "Environmental Engineering",
+        place: "Indian Institute of Technology Kanpur",
+      },
+      {
+        title: "M.S",
+        year: "1993",
+        course: "Environmental Engineering",
+        place: "Indian Institute of Technology Kanpur",
+      },
+      {
+        title: "B.Tech",
+        year: "1990",
+        course: "Civil Engineering",
+        place: "M.G.University Kottayam",
+      },
+    ],
+    papers: [
+      {
+        title:
+          "Role of inner-sphere complexation in phosphate removal by metal–organic frameworks: experimental and theoretical investigation",
+        authors: ["Vaishali Choudhary", "Danil W. Boukhvalov", "Ligy Philip"],
+        publisher: "Environmental Science: Water Research and Technology",
+        year: 2023,
+      },
+      {
+        title:
+          "Feasibility of a Planar Coil-Based Inductive-Capacitive Water Level Sensor with a Quality-Detection Feature: An Experimental Study ",
+        authors: [
+          "Lakshmi Areekath",
+          "Gaurav Lodha",
+          "Subham Kumar Sahana",
+          "Boby George",
+          "Ligy Philip",
+          "Subhas Mukhopadhyay",
+        ],
+        publisher: "Sensors",
+        year: 2022,
+      },
+      {
+        title:
+          "Fate of carbamazepine and its effect on physiological characteristics of wetland plant species in the hydroponic system .",
+        authors: ["Manthiram Karthik Ravichandran", "Ligy Philip"],
+        publisher: "Science of the Total Environment,",
+        year: 2022,
+      },
+    ],
+  },
+];
+
+export default faculty;
