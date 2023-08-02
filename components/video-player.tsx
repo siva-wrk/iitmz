a61c1082ae1472546738e668aacaf3d3fb143c85
Alwin <alwin@Alwins-MacBook-Air.local>
2023-08-02 13:17:10 +0530

    fixed react player ssr hydration issue

--- components/video-player.tsx
+++ components/video-player.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ReactPlayer from "react-player/youtube";
 import Image from "next/image";
 import styles from "./video-player.module.scss";
@@ -11,25 +11,34 @@ type VideoPlayerProps = {
 
 export default function VideoPlayer({ url, previewImage }: VideoPlayerProps) {
   const [previewLoaded, setPreviewLoaded] = useState(false);
+  // Only Render ReactPlayer on Client
+  const [hasWindow, setHasWindow] = useState(false);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setHasWindow(true);
+    }
+  }, []);
   return (
     <div className={!previewLoaded ? styles.skelton : styles.bg}>
-      <ReactPlayer
-        url={url}
-        light={
-          <Image
-            src={`data:image/png;base64,${previewImage}`}
-            alt="Thumbnail Image"
-            fill={true}
-            style={{ objectFit: "cover" }}
-            priority={true}
-            onLoadingComplete={() => setPreviewLoaded(true)}
-          />
-        }
-        height="100%"
-        width="100%"
-        playing={true}
-        controls={true}
-      />
+      {hasWindow && (
+        <ReactPlayer
+          url={url}
+          light={
+            <Image
+              src={`data:image/png;base64,${previewImage}`}
+              alt="Thumbnail Image"
+              fill={true}
+              style={{ objectFit: "cover" }}
+              priority={true}
+              onLoadingComplete={() => setPreviewLoaded(true)}
+            />
+          }
+          height="100%"
+          width="100%"
+          playing={true}
+          controls={true}
+        />
+      )}
     </div>
   );
 }
